75e8208786c692a07eba67dbe873e250a6f68bc8
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 17:26:38 +0100

    Update ClassificationViewModel.kt

--- app/src/main/java/com/example/breastcancerlocal/viewModel/ClassificationViewModel.kt
+++ app/src/main/java/com/example/breastcancerlocal/viewModel/ClassificationViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.breastcancerlocal.viewModel
 
-import android.content.Context
+import android.content.Context 
 import android.content.res.AssetManager
 import androidx.lifecycle.ViewModel
 import com.example.breastcancerlocal.model.Diabeats
